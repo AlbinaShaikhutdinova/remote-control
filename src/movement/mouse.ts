aed220a218161b6a7f74b92a03964ddc6b4b1e93
Albina Shaikhutdinova <ya.shalbina@gmail.com>
2023-01-21 15:16:25 +0300

    feat: add mouse control

---
+++ src/movement/mouse.ts
@@ -0,0 +1,13 @@
+import { mouse, left, right, up, down } from '@nut-tree/nut-js';
+export async function moveRight(offset: number) {
+  await mouse.move(right(offset));
+}
+export async function moveLeft(offset: number) {
+  await mouse.move(left(offset));
+}
+export async function moveUp(offset: number) {
+  await mouse.move(up(offset));
+}
+export async function moveDown(offset: number) {
+  await mouse.move(down(offset));
+}
--- src/ws/websocket.ts
+++ src/ws/websocket.ts
@@ -1,4 +1,6 @@
 import { WebSocketServer } from 'ws';
+// @ts-ignore
+import { moveLeft, moveRight, moveUp, moveDown } from '../movement/mouse.ts';
 
 export const wss = new WebSocketServer({ port: 8080 });
 export const wsInit = () => wss.on('connection', (ws) => connection(ws));
@@ -7,7 +9,7 @@ function connection(ws: {
   on: (arg0: string, arg1: (data: any) => void) => void;
   send: (arg0: string) => void;
 }) {
-  ws.on('message', function message(data) {
+  ws.on('message', async function message(data) {
     console.log('received: %s', data);
     const command = data.toString().split(' ')[0];
     const prop = data.toString().split(' ')[1];
@@ -16,11 +18,20 @@ function connection(ws: {
       const prop2 = data.toString().split(' ')[2];
       response = `${command}_${prop}px_${prop2}px`;
     }
-
     ws.send(response);
     switch (command) {
       case 'mouse_left':
-        ws.send('something');
+        await moveLeft(Number(prop));
+        break;
+      case 'mouse_right':
+        await moveRight(Number(prop));
+        break;
+      case 'mouse_up':
+        await moveUp(Number(prop));
+        break;
+      case 'mouse_down':
+        await moveDown(Number(prop));
+        break;
     }
   });
 }
